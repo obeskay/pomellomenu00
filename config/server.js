6957df883d29c2c7c219fe2d48ed7eca951db9e0
Obed Vargas Villarreal <obedvargasvillarreal@MacBook-Air-de-Obed.local>
2021-11-02 23:25:53 -0600

    Update database config

--- config/server.js
+++ config/server.js
@@ -1,9 +1,10 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  url: env("MY_HEROKU_URL"),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '2a57741fb7c83f540892856a7a01c350'),
+      secret: env("ADMIN_JWT_SECRET", "2a57741fb7c83f540892856a7a01c350"),
     },
   },
 });
